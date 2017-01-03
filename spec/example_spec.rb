f6454a4319333e38bf12569dbd1f32369a6e23f7
kmeister <kris@aliencom.net>
2017-01-02 20:19:17 -0500

    added new test

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -2,4 +2,7 @@ RSpec.describe 'Docker' do
     it 'helps us eliminate differences between CI and dev environments' do
         expect('indeed it does').to be
     end
+    it 'Can run more tests on commit' do
+        expect('indeed it does').to be
+    end
 end
