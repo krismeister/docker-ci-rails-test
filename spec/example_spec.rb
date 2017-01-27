9ed05fcef7e9a9abf886cab0af2fcfb7e87c83a6
kmeister <kris@aliencom.net>
2017-01-26 22:15:20 -0500

    added new test to kris_build branch

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -2,4 +2,7 @@ RSpec.describe 'Docker' do
     it 'helps us eliminate differences between CI and dev environments' do
         expect('indeed it does').to be
     end
+    it 'and we can try another test' do
+        expect('indeed it does').to be
+    end
 end
